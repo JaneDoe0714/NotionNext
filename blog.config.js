329f36a995b52cdf1474156136033833a5522be1
JaneDoe0714 <JaneDoe0714@proton.me>
2026-08-20 02:44:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Pierides undae', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '就这样他的意志和坚实的智慧战胜了；/就这样他旅行到远方，/远离这个世界的烈焰熊熊的墙垒，/直至他游遍了无穷无尽的大宇。', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-63aos7fl3-janedoe0714s-projects.vercel.app', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://pierideundae.cc.cd', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '同人, 个人漫游,电子图书馆', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
